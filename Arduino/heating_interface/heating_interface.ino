8e476c8c02aa2b15044fdc94283f008b83460bed
Cameron Gray <cameron@camerongray.me>
2018-04-06 23:27:17 +0100

    Updated Arduino code to read system's actual voltage to ensure the temperature readings are accurate

--- Arduino/heating_interface/heating_interface.ino
+++ Arduino/heating_interface/heating_interface.ino
@@ -16,8 +16,6 @@
 #define AVG_TEMP_NUM_READINGS 20
 #define AVG_TEMP_READING_DELAY 50
 
-#define ARDUINO_VOLTAGE 5
-
 void setup() {
   pinMode(RELAY_OUT, OUTPUT);
   pinMode(CH_LED_OUT, OUTPUT);
@@ -53,7 +51,7 @@ float averagedTemp() {
 float readTemp() {
   int rawValue = analogRead(TEMP_IN);
   
-  float voltage = (rawValue / 1024.0) * float(ARDUINO_VOLTAGE);
+  float voltage = (rawValue / 1024.0) * (readVcc()/1000.0);
   float temperature = (voltage - 0.5) / 0.01;
   
   return temperature;
@@ -83,4 +81,30 @@ void stat1_led_off() {
 
 void stat2_led_off() {
   digitalWrite(STAT2_LED_OUT, LOW);
+}
+
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
 }
